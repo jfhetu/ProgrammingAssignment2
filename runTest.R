4f860df731b62cf78a5da9f1cce1d377bdfbc47d
jfhetu <jf.hetu@me,com>
2018-07-27 11:12:58 -0400

    Update tests on multiple objects*

--- runTest.R
+++ runTest.R
@@ -1,25 +1,44 @@
 # test cacheMatrix
 
 source("cachematrix.R")
-mat = read.csv("diagmatrix.csv")
-m<-makeCacheMatrix(mat)
-m$get()
+mat1 = read.csv("diagmatrix.csv")
+mat2 = read.csv("diagmatrix2.csv")
+
+m1<-makeCacheMatrix(mat1)
+m2<-makeCacheMatrix(mat2)
+
+m1$get()
 # V1 V2 V3
 # 1  1  0  0
 # 2  0  2  0
 # 3  0  0  3
-m$getInverse()
+m1$getInverse()
 # NULL
 
-cacheSolve(m)
+m2$get()
+# V1 V2 V3
+# 1  1  0  0
+# 2  0  2  0
+# 3  0  0  3
+m2$getInverse()
+# NULL
+
+cacheSolve(m1)
 # [,1] [,2]      [,3]
 # V1    1  0.0 0.0000000
 # V2    0  0.5 0.0000000
 # V3    0  0.0 0.3333333
 
-m$getInverse()
+cacheSolve(m2)
+# [,1] [,2]      [,3]
+# V1    2  0.0 0.0000000
+# V2    0  2 0.0000000
+# V3    0  0.0 2
+
+m1$getInverse()
 #[,1] [,2]      [,3]
 #V1    1  0.0 0.0000000
 #V2    0  0.5 0.0000000
 #V3    0  0.0 0.3333333
  
+
